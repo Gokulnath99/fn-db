0794fc11b5aae9e14389fcc69975c7dfacc01395
Gokulnath99 <gokulnathanand23@gmail.com>
2020-08-17 22:56:31 +0530

    Add Production in knexfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send(This is the DB!);
+    res.send(""This is the DB!");
 });
 
 const port = process.env.PORT || 4444;
--- knexfile.js
+++ knexfile.js
@@ -6,5 +6,10 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://postgres:khuljaa@localhost/faculty-network',
     // connection: { user: 'postgres', database: 'faculty-network', password: 'khuljaa' }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    // connection: { user: 'postgres', database: 'faculty-network', password: 'khuljaa' }
   }
 };
